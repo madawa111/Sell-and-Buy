285af91886e5bdfe76c3b2b3687ba89573546edf
madawa111 <madawa3445@gmail.com>
2021-10-29 00:17:01 +0530

    edited

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -127,7 +127,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
       _isLoading = false;
     });
     Navigator.of(context).pop();
-    // Navigator.of(context).pop();
+    //
   }
 
   @override
